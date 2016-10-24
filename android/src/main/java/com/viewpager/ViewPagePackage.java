a9e709334c1d82331131d8fb25c4900e73a31356
dengzhi@uvoplus.com <dengzhi@uvoplus.com>
2016-10-24 18:23:48 +0800

    fix bug add package

---
+++ android/src/main/java/com/viewpager/ViewPagePackage.java
@@ -0,0 +1,40 @@
+package com.viewpager;
+
+/**
+ * Created by azou on 15/02/16.
+ */
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.viewpager.ReactViewPagerManager;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * RNMK Package declaration.
+ *
+ * Created by ywu on 15/9/23.
+ */
+public class ViewPagePackage implements ReactPackage {
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
+      return Arrays.<ViewManager>asList(
+          new ReactViewPagerManager()
+      );
+  }
+}
